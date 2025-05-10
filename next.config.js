a7c5b646a4beefa898a694d06401f0dabdd4f54f
daedong-church <scpark@knuh.kr>
2025-05-10 18:35:14 +0900

    Build for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/pray',  // 저장소 이름이 'pray'이므로 소문자로 수정
-  assetPrefix: '/pray/',
-  trailingSlash: true
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: '/pray',
+    assetPrefix: '/pray/',
+    trailingSlash: true,
+  }
+  module.exports = nextConfig;
